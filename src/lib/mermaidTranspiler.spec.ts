aefb2381a152c23d39c27d569ae2bee763ebce53
Alexandre Zenon <ahzed11@protonmail.com>
2023-08-19 14:26:19 +0200

    Add more complex test

--- src/lib/mermaidTranspiler.spec.ts
+++ src/lib/mermaidTranspiler.spec.ts
@@ -30,6 +30,53 @@ test('simple', (t) => {
   const parser = new Parser(lexer.tokens);
   const transpilation = new MermaidTranspiler().generates(parser.statements);
 
+  t.is(transpilation, expect);
+});
+
+test('harder', (t) => {
+  const input = '\n' +
+    'Fact purchase {\n' +
+    '  int id;\n' +
+    '  int item;\n' +
+    '  int price;\n' +
+    '  int customer;\n' +
+    '}\n' +
+    '\n' +
+    'Dimension customer {\n' +
+    '  int id -> purchase.customer;\n' +
+    '  text name;\n' +
+    '}\n' +
+    '\n' +
+    'Dimension item {\n' +
+    '  int id -> purchase.item;\n' +
+    '  text name;\n' +
+    '}\n';
+
+  const expect = 'erDiagram\n' +
+    '\n' +
+    'purchase {\n' +
+    '\tint id\n' +
+    '\tint item\n' +
+    '\tint price\n' +
+    '\tint customer\n' +
+    '}\n' +
+    '\n' +
+    'customer ||--|{ purchase:customer\n' +
+    'customer {\n' +
+    '\tint id\n' +
+    '\ttext name\n' +
+    '}\n' +
+    '\n' +
+    'item ||--|{ purchase:item\n' +
+    'item {\n' +
+    '\tint id\n' +
+    '\ttext name\n' +
+    '}\n\n';
+
+  const lexer = new Lexer(input);
+  const parser = new Parser(lexer.tokens);
+  const transpilation = new MermaidTranspiler().generates(parser.statements);
+
   console.log(transpilation);
 
   t.is(transpilation, expect);
